30a6abb5891c87c2e3f5fab6a2c68528d3bd91b5
Prateek Pandey <orion.prateek@gmail.com>
2017-09-18 10:49:44 +0530

    Added files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
+
+app.post('/test', function(req, res) {
+    var speech = 'This is a test'
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-test-sample'
+    });
+});
+
+app.listen((process.env.PORT || 8000), function() {
+    console.log("Server up and listening");
+});
