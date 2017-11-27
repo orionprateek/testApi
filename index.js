cde450696f9b5a4d0deb0c51c2698a2adb5377c6
Prateek Pandey <orion.prateek@gmail.com>
2017-11-27 15:56:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,23 +17,37 @@ app.post('/test', function(req, res) {
       , intent = req.body.result && req.body.result.metadata.intentName ? req.body.result.metadata.intentName : "noIntent";
     if(intent === 'checkWebhookUrl'){
         speech = "This is Webhook URL test." 
-        var tempOb = {
-                          "type": "link_out_chip",
-                          "platform": "google",
-                          "destinationName": "Video",
-                          "url": "https://www.youtube.com/watch?v=1rMm0qaa2Hs"
-                     }
-        messages.push(tempOb);
+        var tempOb1 = {
+          "type": "simple_response",
+          "platform": "google",
+          "textToSpeech": "Please visit this link.",
+          "displayText": "Please visit this link."
+        }
+        var tempOb2 = {
+          "type": "link_out_chip",
+          "platform": "google",
+          "destinationName": "Video",
+          "url": "https://www.youtube.com/watch?v=1rMm0qaa2Hs"
+        }
+        var tempOb3 = {
+          "type": 0,
+          "speech": ""
+        }        
+        messages.push(tempOb1);
+        messages.push(tempOb2);
+        messages.push(tempOb3);
+        return res.json({
+        speech: ''
+        messages: messages
     }
     else{
         speech = "No intent Found!"
-    }
-    return res.json({
+        return res.json({
         speech: speech,
         displayText: speech,
-        source: 'webhook-test-sample',
-        messages: messages
+        source: 'webhook-test-sample'
     });
+    }
 });
 
 app.listen((process.env.PORT || 8000), function() {
