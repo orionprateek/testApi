db02c41f744dff779d811b1986f81246910fd0bf
Prateek Pandey <orion.prateek@gmail.com>
2017-11-27 14:59:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 
 app.post('/test', function(req, res) {
     var speech = 'This is a test'
+      , intent = req.body.result && req.body.result.metadata.intentName ? req.body.result.metadata.intentName : "noIntent";
     return res.json({
         speech: speech,
         displayText: speech,
